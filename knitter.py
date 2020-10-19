4e8856dd7955645c08f635f34e977cf15e6adf7a
Mert Gör <mertgor@masscollabs.com>
2020-10-19 08:47:27 +0300

    knitter main py update

--- knitter.py
+++ knitter.py
@@ -5,7 +5,7 @@
 #    Knitter runs as a server and provided services to network users so we have choosen AGPLv3 or any later license fot that purpose
 #    Enjoy it! Happy hacking!
 #
-#    Copyright (C) 2020,  Masscollabs Services, hwpplayers, procyberian(procyberian-linux), amassivus
+#    Copyright (C) 2020 Masscollabs Services, Masscollaboration Labs, amassivus, procyberian, hwpplayers
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -31,7 +31,7 @@ print ("""
      Knitter runs as a server and provided services to network users so we have choosen AGPLv3 or any later license fot that purpose
      Enjoy it! Happy hacking!
 
-     Copyright (C) 2020,  Masscollabs Services, hwpplayers, procyberian(procyberian-linux), amassivus
+     Copyright (C) 2020 Masscollabs Services, Masscollaboration Labs, amassivus, procyberian, hwpplayers
  
      This program is free software: you can redistribute it and/or modify
      it under the terms of the GNU Affero General Public License as published
